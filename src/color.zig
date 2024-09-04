3462b9dfc42d63d524ba4a2a9563dbd426674b99
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-09-04 10:27:45 -0400

    Update Color type name

--- src/color.zig
+++ src/color.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 const zm = @import("zmath");
 
-pub const color = @Vector(3, f32);
+pub const Color = @Vector(4, f32);
 
-pub fn x(pixel_color: color) f32 {
+pub fn x(pixel_color: Color) f32 {
     return pixel_color[0];
 }
 
-pub fn y(pixel_color: color) f32 {
+pub fn y(pixel_color: Color) f32 {
     return pixel_color[1];
 }
 
-pub fn z(pixel_color: color) f32 {
+pub fn z(pixel_color: Color) f32 {
     return pixel_color[2];
 }
 
-pub fn writeColor(pixel_color: color, writer: anytype) !void {
+pub fn writeColor(pixel_color: Color, writer: anytype) !void {
     const r = x(pixel_color);
     const g = y(pixel_color);
     const b = z(pixel_color);
@@ -25,4 +25,8 @@ pub fn writeColor(pixel_color: color, writer: anytype) !void {
     const ib: u32 = @intFromFloat(255.999 * b);
 
     try std.fmt.format(writer, "{d} {d} {d}\n", .{ ir, ig, ib });
+}
+
+pub fn splat(value: f32) Color {
+    return @splat(value);
 }
